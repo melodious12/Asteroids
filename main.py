fa6e722f62a4b648024a529c294621f4ee1760f4
melodious12 <zjulian10@gmail.com>
2024-11-14 18:58:36 -0500

    updated main to limit frame rate

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from constants import *
 
 def main():
 	pygame.init
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		screen.fill((0,0,0))
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
