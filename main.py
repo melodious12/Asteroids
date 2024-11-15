79b8086f9c38e252d3e083fbc742f94523fa129b
melodious12 <zjulian10@gmail.com>
2024-11-14 21:22:04 -0500

    .

--- main.py
+++ main.py
@@ -7,21 +7,27 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
-	player.containers = (updatable, drawable)
-	while True:
+	Player.containers = (updatable, drawable)
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+	running = True
+	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
     
-		dt = clock.tick(60) / 1000
-		updatable.update(dt)
+		for entity in updatable:
+			entity.update()
 
 		screen.fill("black")
-		drawable.draw(screen)
+
+		for entity in drawable:
+			entity.draw(screen)
+
 		pygame.display.flip()
+
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
