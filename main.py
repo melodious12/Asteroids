9c5a11bc622619e6e946a586c09780d538efc6a7
melodious12 <zjulian10@gmail.com>
2024-11-14 22:12:37 -0500

    added collision method to close game if collision between player and asteroid

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+import sys
 import pygame
 from constants import *
-from player import *
+from player import Player
 from asteroid import Asteroid
-from asteroidfield import *
+from asteroidfield import AsteroidField
 
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+
 	Player.containers = (updatable, drawable)
 	Asteroid.containers = (asteroids, updatable, drawable)
 	AsteroidField.containers = (updatable)
+
 	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 	asteroid_field = AsteroidField()
+
 	running = True
 	while running:
 		for event in pygame.event.get():
@@ -25,6 +30,11 @@ def main():
     
 		for entity in updatable:
 			entity.update(dt)
+
+		for asteroid in asteroids:
+			if asteroid.collides_with(player):
+				print("Game over!")
+				sys.exit()
 
 		screen.fill("black")
 
