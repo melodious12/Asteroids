00fab8ce4637505d6a4a867de9c8690ae0ba6876
melodious12 <zjulian10@gmail.com>
2024-11-14 23:30:40 -0500

    .

--- shot.py
+++ shot.py
@@ -6,9 +6,13 @@ class Shot(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, SHOT_RADIUS)
         self.add(self.containers)
+        self.position = pygame.math.Vector2(x, y)
+        self.rect = pygame.Rect(self.position.x, self.position.y, SHOT_RADIUS*2, SHOT_RADIUS*2)
+        self.velocity = pygame.math.Vector2(0, PLAYER_SHOOT_SPEED).rotate(self.position)
 
     def draw(self, screen):
         pygame.draw.circle(screen, "blue", self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+        self.rect.center = self.position
