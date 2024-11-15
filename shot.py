279412f8890c2d646ed94bd70df67ef77323dc67
melodious12 <zjulian10@gmail.com>
2024-11-15 00:17:19 -0500

    .

--- shot.py
+++ shot.py
@@ -3,7 +3,7 @@ from circleshape import CircleShape
 from constants import *
 
 class Shot(CircleShape):
-    def __init__(self, x, y, direction):
+    def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screen):
