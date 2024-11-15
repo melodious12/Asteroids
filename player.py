f5bba7c93be254ba1a5efd2377f5c05bfe90ba40
melodious12 <zjulian10@gmail.com>
2024-11-14 23:55:52 -0500

    .

--- player.py
+++ player.py
@@ -28,10 +28,12 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        global shots
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         direction = pygame.Vector2(0, -1)
         direction = direction.rotate(self.rotation)
         shot.velocity = direction * PLAYER_SHOOT_SPEED
+        shots.add(shot)
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
