c5f1f704074ac9690172f1dba8a6f4daff801d9f
melodious12 <zjulian10@gmail.com>
2024-11-14 23:17:10 -0500

    .

--- player.py
+++ player.py
@@ -30,7 +30,7 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         direction = pygame.Vector2(0, -1)
-        direction = direction.rotate(self.angle)
+        direction = direction.rotate(self.rotation)
         shot.velocity = direction * PLAYER_SHOOT_SPEED
     
     def update(self, dt):
