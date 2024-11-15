78825034915f60b6234d381627634e2d33b47fe2
melodious12 <zjulian10@gmail.com>
2024-11-14 19:38:31 -0500

    created player file and updated main

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
 	pygame.init
 	clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 	while True:
 		screen.fill((0,0,0))
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
+		player.draw(screen)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
